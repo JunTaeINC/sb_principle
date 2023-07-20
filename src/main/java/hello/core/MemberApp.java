f9f15f3a597fef3bb83fa18c17db961e639ee4d2
JunTaeINC <juntea3523@naver.com>
2023-07-20 23:15:34 +0900

    feat: 스프링으로 전환

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.config.AppConfig;
+import hello.core.member.domain.Grade;
+import hello.core.member.domain.Member;
+import hello.core.member.service.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("New Member = " + member.getName());
+        System.out.println("Find Member = " + findMember.getName());
+    }
+}
--- src/main/java/hello/core/config/AppConfig.java
+++ src/main/java/hello/core/config/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
 import hello.core.order.service.OrderService;
 import hello.core.order.service.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
 }
