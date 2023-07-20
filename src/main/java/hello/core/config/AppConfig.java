b0658b7bca2e96912e9f7e44b42de656eee65eb9
JunTaeINC <juntea3523@naver.com>
2023-07-20 16:39:03 +0900

    refactor: AppConfig 중복제거, 역할 / 구현 나누기

--- src/main/java/hello/core/config/AppConfig.java
+++ src/main/java/hello/core/config/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core.config;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.repository.MemberRepository;
 import hello.core.member.repository.MemoryMemberRepository;
 import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
@@ -10,10 +12,18 @@ import hello.core.order.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
