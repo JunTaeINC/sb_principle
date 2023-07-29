a58049f0e7168c98972359385d04ab2beb4f3bd9
JunTaeINC <juntea3523@naver.com>
2023-07-29 16:03:00 +0900

    test: 싱글톤 적용 테스트

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,8 +2,9 @@ package hello.core.singleton;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.config.AppConfig;
 import hello.core.member.service.MemberService;
@@ -26,5 +27,15 @@ public class SingletonTest {
 		SingletonService instance2 = SingletonService.getInstance();
 
 		assertThat(instance1).isSameAs(instance2);
+	}
+
+	@Test
+	void springContainer() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		MemberService memberService_1 = ac.getBean("memberService", MemberService.class);
+		MemberService memberService_2 = ac.getBean("memberService", MemberService.class);
+
+		assertThat(memberService_1).isSameAs(memberService_2);
 	}
 }
