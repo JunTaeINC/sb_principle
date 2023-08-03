0fc14dc22ce9143178d50c0b463394ee2ad8fb9f
JunTaeINC <juntea3523@naver.com>
2023-08-03 23:25:27 +0900

    refactor: Use Scope Option 'proxyMode'

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -5,21 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hello.core.common.MyLogger;
-import jakarta.inject.Provider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-	private final Provider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 	private final LogDemoService logDemoService;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestUrl = request.getRequestURL().toString();
-		MyLogger myLogger = myLoggerProvider.get();
 
 		myLogger.setRequestUrl(requestUrl);
 		myLogger.log("Controller test");
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -3,16 +3,14 @@ package hello.core.web;
 import org.springframework.stereotype.Service;
 
 import hello.core.common.MyLogger;
-import jakarta.inject.Provider;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-	private final Provider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 
 	public void logic(String id) {
-		MyLogger myLogger = myLoggerProvider.get();
 		myLogger.log("Service Id = " + id);
 	}
 }
